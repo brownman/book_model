b18c710f9c6062dcdd260495eba95c575008c475
brownman <ofer.shaham@gmail.com>
2011-09-08 10:30:45 +0300

    act_as_list plugin works

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test3::Application.config.secret_token = 'b70fe5f78b3db58719bc439bc951293a75fcc971e87bfbacab79e331917005576c1340fbb9e72471b3d22525a8e204461362bc97a3da881b031bed16073d4b24'
